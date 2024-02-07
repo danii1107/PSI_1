9e04de7cb54a1ad128e5d9c7cdf158d99638d0e9
enriquegf506 <enriquegf506@gmail.com>
2024-02-07 17:46:02 +0100

    new test for modelsAuthor

--- locallibrary/catalog/tests/test_first_week.py
+++ locallibrary/catalog/tests/test_first_week.py
@@ -7,6 +7,7 @@ EPS-UAM 2024
 import sys
 import os
 from django.test import TestCase
+from catalog import urls
 
 from catalog.models import Book, BookInstance, Language, Genre, Author
 
@@ -109,3 +110,34 @@ class FirstWeekTests(TestCase):
         except Exception:
             print('Did you work on the Language Model Challenge? Something is wrong with that!')
             raise
+
+class AuthorModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Author.objects.create(first_name='Big', last_name='Bob')
+
+    def test_first_name_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEqual(field_label, 'first name')
+
+    def test_date_of_death_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_death').verbose_name
+        self.assertEqual(field_label, 'Died')
+
+    def test_first_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEqual(max_length, 100)
+
+    def test_object_name_is_last_name_comma_first_name(self):
+        author = Author.objects.get(id=1)
+        expected_object_name = f'{author.last_name}, {author.first_name}'
+        self.assertEqual(str(author), expected_object_name)
+
+    #def test_get_absolute_url(self):
+        #author = Author.objects.get(id=1)
+        # This will also fail if the urlconf is not defined.
+        #self.assertEqual(author.get_absolute_url(), '/catalog/author/1') 
