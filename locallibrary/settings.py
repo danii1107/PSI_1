55cd910bd96d463b36fc725c4c4da91d3a67c34b
danii1107 <danielbarsan72003@gmail.com>
2024-02-19 19:12:01 +0100

    render #4

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -26,12 +26,12 @@ load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-mmj&4u9bcd0=@x)gv08m7%x25dzes6^2@sm9t$$g6$&4_$xb5e')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-mmj&4u9bcd0=@x)gv08m7%x25dzes6^2@sm9t$$g6$&4_$xb5e')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['psi-1-qptd-onrender.com']
 
 STATIC_URL = 'static/'
 
