d3baa804f75d534b0128d4e0c4c9163e3b38fbe4
danii1107 <danielbarsan72003@gmail.com>
2024-02-19 20:31:22 +0100

    why isnt render creating su x3

--- build.sh
+++ build.sh
@@ -6,4 +6,7 @@ pip3 install -r requirements.txt
 
 python3.11 manage.py collectstatic --no-input
 python3.11 manage.py migrate
-python3.11 manage.py shell < catalog/images/commands/createsu.py
+export DJANGO_SUPERUSER_EMAIL=alumnodb@alumnodb.com
+export DJANGO_SUPERUSER_USERNAME=alumnodb
+export DJANGO_SUPERUSER_PASSWORD=alumnodb 
+python3.11 manage.py createsuperuser --noinput
