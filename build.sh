065231cf9cbfef9c4fa31188300cfc5b7e1096a3
danii1107 <danielbarsan72003@gmail.com>
2024-02-28 16:27:58 +0100

    2nd chance 4

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ pip3 install -r requirements.txt
 python3.11 manage.py collectstatic --no-input
 python3.11 manage.py migrate
 python3.11 manage.py migrate
-python3.11 manage.py flush
+python3.11 manage.py flush --no-input
 python3.11 populate_catalog.py
 python3.11 manage.py createsuperuser 
