70200019dcb37df5ce988be6e8cc2f886b13cd5c
danii1107 <danielbarsan72003@gmail.com>
2024-02-28 16:23:13 +0100

    2nd chance 2

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ pip3 install -r requirements.txt
 
 python3.11 manage.py collectstatic --no-input
 python3.11 manage.py migrate
-python3.11 manage.py flush
+python3.11 manage.py migrate
+spawn "python3.11 manage.py flush"
+expect "Are you sure you want to do this?"
+send "yes"
+expect eof
 python3.11 populate_catalog.py
 python3.11 manage.py createsuperuser 
